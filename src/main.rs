dd25ff51a9828751f04a9f54c17f9df1c99fe43a
megabyte112 <74556753+megabyte112@users.noreply.github.com>
2023-01-15 22:00:36 +0000

    add cli mode

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
 fn main() {
-    println!("toki pona translator");
-    println!("type 'q' or 'quit' to exit\n");
-    loop {
-        println!("write sentence:");
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).expect("readline failed");
-        println!();
-        let input = input.trim().to_lowercase();
-        if input == "q" || input == "quit" {
-            break;
-        }
-        let words: Vec<&str> = input.split_whitespace().collect();
-        for word in words {
-            let mut word = word.to_string();
-            while word.len() < 12 {
-                word.push(' ');
+    // check if argument is passed
+    if std::env::args().len() == 1 {
+        println!("toki pona translator");
+        println!("type 'q' or 'quit' to exit\n");
+        loop {
+            println!("write sentence:");
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).expect("readline failed");
+            println!();
+            let input = input.trim().to_lowercase();
+            if input == "q" || input == "quit" {
+                break;
             }
-            let word = word.as_str();
-            println!("{}:\t{}", word, translateword(word));
+            let words: Vec<&str> = input.split_whitespace().collect();
+            let words: Vec<String> = words.iter().map(|&x| x.to_string()).collect();
+            output(&words);
+            println!();
         }
-        println!();
+    }
+    else {
+        let mut args = std::env::args();
+        args.next();
+        let words: Vec<String> = args.collect();
+        output(&words);
     }
 }
 fn translateword(word: &str) -> &str {
@@ -29,4 +32,14 @@ fn translateword(word: &str) -> &str {
     let wordlist = wordlist.map(|line| line.split("-").collect::<Vec<&str>>());
     let wordlist = wordlist.map(|line| (line[0], line[1])).collect::<std::collections::HashMap<&str, &str>>();
     wordlist.get(&*word).unwrap_or(&"<no word found>")
+}
+fn output(args: &Vec<String>) {
+    for word in args {
+        let mut word = word.to_string();
+        while word.len() < 12 {
+            word.push(' ');
+        }
+        let word = word.as_str();
+        println!("{}:\t{}", word, translateword(word));
+    }
 }
